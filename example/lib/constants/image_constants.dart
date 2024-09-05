a328ae6c578838f6f83bb8a775c502deb42f664b
DAArbelaez <diego.arbelaez@venko.co>
2024-09-04 19:37:37 -0500

    Add image constants

---
+++ example/lib/constants/image_constants.dart
@@ -0,0 +1,10 @@
+const _imagesSvgPath = "assets/images/svg/";
+const _animationsPath = "assets/animations/";
+
+// SVG
+const kCarErrorImageSvg = "${_imagesSvgPath}cm_car_error.svg";
+const kSuccessImage = "${_imagesSvgPath}success.svg";
+const kCarImage = "${_imagesSvgPath}car.svg";
+
+// Animations
+const kLoadingAnimation = "${_animationsPath}cmloading.json";
