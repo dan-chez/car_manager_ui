a196bb2d64eb471e55a531fd2743c23d4c591894
Daniel Sanchez <44003827+dan-chez@users.noreply.github.com>
2025-02-19 07:46:30 -0500

    CMDateButton moved to package (#50)

--- lib/carmanager_ui.dart
+++ lib/carmanager_ui.dart
@@ -15,6 +15,7 @@
 library carmanager_ui;
 
 export 'package:carmanager_ui/src/components/buttons/cm_icon_button.dart';
+export 'package:carmanager_ui/src/components/buttons/cm_date_button.dart';
 export 'package:carmanager_ui/src/components/buttons/primary_button.dart';
 export 'package:carmanager_ui/src/components/buttons/close_icon_button.dart';
 export 'package:carmanager_ui/src/components/cm_appbar.dart';
