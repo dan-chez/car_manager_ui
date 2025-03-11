2981c2c1884a283d3e14d88bd4c4258893f09ade
Daniel Sanchez <danielmsa96@gmail.com>
2025-03-11 10:32:03 -0500

    SelectableServiceType and SelectableVehicleType use CMDimens

--- lib/src/components/selectable_vehicle_type/selectable_vehicle_type.dart
+++ lib/src/components/selectable_vehicle_type/selectable_vehicle_type.dart
@@ -14,6 +14,7 @@
 
 import 'package:carmanager_ui/src/components/selectable_vehicle_type/selectable_vehicle_type_item.dart';
 import 'package:carmanager_ui/src/constants/app_colors_constants.dart';
+import 'package:carmanager_ui/src/constants/cm_dimens.dart';
 import 'package:carmanager_ui/src/constants/cm_icons.dart';
 import 'package:carmanager_ui/src/constants/text_style_constants.dart';
 import 'package:flutter/material.dart';
@@ -55,9 +56,9 @@ class SelectableVehicleType extends StatefulWidget {
 
 class _SelectableVehicleTypeState extends State<SelectableVehicleType> {
   /// Spacing between items.
-  static const double _wrapSpacing = 8;
+  static const double _wrapSpacing = CMDimens.d8;
 
-  static const double _iconSize = 24;
+  static const double _iconSize = CMDimens.d24;
 
   String? selectedId;
 
@@ -81,26 +82,28 @@ class _SelectableVehicleTypeState extends State<SelectableVehicleType> {
               },
               child: Container(
                 width: containerWidth,
-                constraints: const BoxConstraints(minWidth: 80),
+                constraints: const BoxConstraints(minWidth: CMDimens.d80),
                 decoration: BoxDecoration(
                   color: kWhite,
                   boxShadow: const [
                     BoxShadow(
                       color: kBoxShadowColor,
-                      blurRadius: 4,
-                      offset: Offset(0, 5),
+                      blurRadius: CMDimens.d4,
+                      offset: Offset(CMDimens.d0, CMDimens.d5),
                     )
                   ],
-                  borderRadius: BorderRadius.circular(6),
+                  borderRadius: BorderRadius.circular(CMDimens.d6),
                 ),
                 child: Container(
                   decoration: BoxDecoration(
                     color:
                         isSelected ? kMyrtleGreen : kPrimaryColorWithOpacityBG,
-                    borderRadius: BorderRadius.circular(6),
+                    borderRadius: BorderRadius.circular(CMDimens.d6),
                   ),
-                  padding:
-                      const EdgeInsets.symmetric(vertical: 14, horizontal: 12),
+                  padding: const EdgeInsets.symmetric(
+                    vertical: CMDimens.d14,
+                    horizontal: CMDimens.d12,
+                  ),
                   child: Row(
                     mainAxisSize: MainAxisSize.min,
                     children: [
@@ -113,7 +116,7 @@ class _SelectableVehicleTypeState extends State<SelectableVehicleType> {
                         width: _iconSize,
                         height: _iconSize,
                       ),
-                      const SizedBox(width: 8),
+                      const SizedBox(width: CMDimens.d8),
                       Expanded(
                         child: Text(
                           item.description,
