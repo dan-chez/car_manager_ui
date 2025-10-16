1499fa3102fccb0dcb7d319d0119682668bfa0e9
DArbelaez <diegoandresarbelaez@gmail.com>
2025-10-15 21:43:29 -0500

    Add missing test

--- test/src/components/text_field/money_text_field/money_text_editing_controller_test.dart
+++ test/src/components/text_field/money_text_field/money_text_editing_controller_test.dart
@@ -1,3 +1,17 @@
+// Copyright 2024 Danchez
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 import 'package:carmanager_ui/src/components/text_field/money_text_field/money_text_editing_controller.dart';
 import 'package:flutter_test/flutter_test.dart';
 
@@ -88,6 +102,15 @@ void main() {
         () => CMMoneyTextController(rightSymbol: 'USD1'),
         throwsArgumentError,
       );
+    });
+
+    test('executes super.value branch when already formatting', () {
+      final controller = CMMoneyTextController();
+
+      final value = controller.value;
+      controller.value = value;
+      controller.value = value;
+      expect(controller.text, isNotEmpty);
     });
   });
 }
